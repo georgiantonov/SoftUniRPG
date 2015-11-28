e9e5d8436a964d10fd7b7cbe319c9ef20617919f
ROSSFilipov <ruslanfilipov@yahoo.com>
2015-11-28 18:20:49 +0200

    Fixes done to item slot related classes

--- GameObjects/Inventory/InventoryValues.cs
+++ GameObjects/Inventory/InventoryValues.cs
@@ -9,6 +9,13 @@ namespace Game.GameObjects.Inventory
     //A static class holding all inventory-related constant values
     public static class InventoryValues
     {
-        public const int BackpackBaseSlotNumber = 10;
+        public const int BackpackBaseNumberOfSlots = 10;
+
+        public const int InventoryMainHandSlotNumber = 1;
+        public const int InventoryOffHandSlotNumber = 2;
+        public const int InventoryChesArmorSlotNumber = 3;
+        public const int InventoryHeadArmorSlotNumber = 4;
+        public const int InventoryFeetArmorSlotNumber = 5;
+        public const int InventoryHandArmorSlotNumber = 6;
     }
 }
--- GameObjects/Inventory/ItemSlots/ItemSlot.cs
+++ GameObjects/Inventory/ItemSlots/ItemSlot.cs
@@ -1,4 +1,5 @@
-﻿using Game.Interfaces;
+﻿using Game.GameObjects.Items;
+using Game.Interfaces;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,6 +10,47 @@ namespace Game.GameObjects.Inventory
 {
     public abstract class ItemSlot : IItemSlot
     {
+        private IItem item;
+        private bool isEmpty;
+        private int slotNumber;
 
+        public ItemSlot()
+        {
+            this.isEmpty = true;
+        }
+
+        public IItem Item
+        {
+            get
+            {
+                return this.item;
+            }
+            set
+            {
+                this.item = value;
+            }
+        }
+
+        public bool IsEmpty
+        {
+            get
+            {
+                return this.isEmpty;
+            }
+        }
+
+        public int SlotNumber
+        {
+            get
+            {
+                return this.slotNumber;
+            }
+            protected set
+            {
+                this.slotNumber = value;
+            }
+        }
+
+        public abstract void EquipItem(IItem itemToBeEquipped);
     }
 }
