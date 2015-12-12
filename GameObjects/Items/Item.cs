81f083e62f732660d0ec47e447ea57a7702b99aa
IvayloGoranov <ivaylo.v.goranov@gmail.com>
2015-12-12 15:49:23 +0200

    Adding functionality to Items.cs

--- GameObjects/Items/Item.cs
+++ GameObjects/Items/Item.cs
@@ -1,13 +1,21 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Game.GameObjects.Items;
+using Game.Interfaces;
 
 namespace Game.GameObjects.Items
 {
-    public abstract class Item
+    public abstract class Item : GameObject, IItem
     {
-        
+        public Item(Point location, Image image, int id)
+            : base(location, image, id)
+        {
+            this.ItemState = ItemState.Available;
+        }
+        public ItemState ItemState { get; set; }
     }
 }
