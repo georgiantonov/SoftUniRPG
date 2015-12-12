60a878681032c79785836d9c76302b436bb84153
IvayloGoranov <ivaylo.v.goranov@gmail.com>
2015-12-12 13:40:41 +0200

    Editing WorldMapSprite class to inherit base class GameObject

--- Entities/WorldMapSprite.cs
+++ Entities/WorldMapSprite.cs
@@ -1,40 +1,24 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
 using System.Drawing;
 using System.Windows.Forms;
+using Game.GameObjects;
 
 namespace Game
 {
-    public class WorldMapSprite
+    //A base(parent) class for all objects that represent living characters/creatures.
+    public class WorldMapSprite : GameObject
     {
-        private Point location;
-        private Image image;
-        private int ID;
-
-        public WorldMapSprite(Point location, Image image, int ID)
+        public WorldMapSprite(Point location, Image image, int id)
+            : base(location, image, id)
         {
-            this.location = location;
-            this.image = image;
-            this.ID = ID;
         }
-
-        // Draw() method
-        // Draws an image at specified location
-        // The location is a Point object - it has a location.X and location.Y properties
-        public void Draw(Graphics device, int x, int y)
-        {
-            location.X = x;
-            location.Y = y;
-            device.DrawImage(image, location);
-        }
-
         public void Move(int x, int y)
         {
-            location.X = x;
-            location.Y = y;
+            this.Location = new Point(x, y);
         }
     }
 }
